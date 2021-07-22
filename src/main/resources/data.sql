d1ca1d83de5ff3a3e4b66e2161d47c4236002012
Maksimgrom94 <maksim_grom@mail.ru>
2021-07-22 16:31:09 +0300

    change sql-queries

---
+++ src/main/resources/data.sql
@@ -0,0 +1,13 @@
+INSERT INTO students (first_name, last_name, phone, address, email, gender, postalCode, education, birthDate) 
+VALUES ('Petr', 'Orlov', '888005353535', 'Empty Street 8', '1@owl.com', 'MALE', '999', 'General secondary education', '1994-3-3');
+
+
+
+--		students.add(new Student('Oleg', 'Krasnov', '2247582', 'Empty Street 8-2', '2@owl.com', Gender.MALE, '999',
+--				'General secondary education', LocalDate.of(1994, 5, 13)));
+--		students.add(new Student('Margot', 'Robbie', '9999999999', 'Holywood Street 1', '3@owl.com', Gender.FEMALE, '254826',
+--				'General tecnical education', LocalDate.of(1990, 2, 7)));
+--		students.add(new Student('Kim', 'Cattrall', '(312)-555-0690', 'Virtual Reality Capsule no 2', '4@owl.com',
+--				Gender.FEMALE, '12345', 'College education', LocalDate.of(1956, 8, 21)));
+--		students.add(new Student('Thomas', 'Anderson', '(312)-555-5555', 'Virtual Reality Capsule no 3', '5@owl.com',
+--				Gender.MALE, '12345', 'College education', LocalDate.of(1962, 3, 11)));
