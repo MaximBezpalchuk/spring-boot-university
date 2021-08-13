1e507c3925d1c77320471ad42dea5af7f3c070e6
Maksimgrom94 <maksim_grom@mail.ru>
2021-08-13 12:17:31 +0300

    add DataSourceTransactionManager

--- src/main/java/com/foxminded/university/config/SpringConfig.java
+++ src/main/java/com/foxminded/university/config/SpringConfig.java
@@ -10,6 +10,7 @@ import org.springframework.context.annotation.PropertySource;
 import org.springframework.context.support.PropertySourcesPlaceholderConfigurer;
 import org.springframework.core.io.Resource;
 import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.datasource.DataSourceTransactionManager;
 import org.springframework.jdbc.datasource.DriverManagerDataSource;
 import org.springframework.jdbc.datasource.init.ResourceDatabasePopulator;
 
@@ -63,6 +64,11 @@ public class SpringConfig {
 	@Bean
 	public static PropertySourcesPlaceholderConfigurer propertySourcesPlaceholderConfigurer() {
 		return new PropertySourcesPlaceholderConfigurer();
+	}
+
+	@Bean
+	public DataSourceTransactionManager txManager() {
+		return new DataSourceTransactionManager(dataSource());
 	}
 
 }
