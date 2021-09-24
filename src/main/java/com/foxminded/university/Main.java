a696058d28f071541c3ca3579dddf89ebec04f80
Maksimgrom <maksim_grom@mail.ru>
2021-09-24 19:00:05 +0300

    add logger reviews by levels

--- src/main/java/com/foxminded/university/Main.java
+++ src/main/java/com/foxminded/university/Main.java
@@ -1,8 +1,14 @@
 package com.foxminded.university;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 public class Main {
 
+	private final static Logger logger = LoggerFactory.getLogger(Main.class);
+
 	public static void main(String[] args) {
+		logger.info("Start programm");
 		MenuCreator menu = new MenuCreator();
 		menu.buildMenu();
 	}
