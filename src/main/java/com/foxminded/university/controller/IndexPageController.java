c09c9004442041d89e4fc8e43e4d7b1be77455d8
user <user@WIN-9Q7LVI4CKHN>
2021-10-04 09:55:14 +0300

    delete unused controller

--- src/main/java/com/foxminded/university/controller/IndexPageController.java
+++ src/main/java/com/foxminded/university/controller/IndexPageController.java
@@ -1,6 +1,5 @@
 package com.foxminded.university.controller;
 
-import org.springframework.http.MediaType;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
@@ -11,10 +10,4 @@ public class IndexPageController {
 	public String index() {
 		return "index";
 	}
-	
-	@GetMapping(value="/hello", produces = MediaType.TEXT_PLAIN_VALUE)
-    public String sayHello() {
-
-        return "Hello there!";
-    }
 }
