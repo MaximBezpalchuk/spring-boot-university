1a395d4745dcad6f095709f17f04070566c4268f
Maksimgrom <hjfhjfhjf12>
2021-12-25 22:32:23 +0300

    optimize imports

--- src/main/java/com/foxminded/university/config/ApplicationConfig.java
+++ src/main/java/com/foxminded/university/config/ApplicationConfig.java
@@ -1,17 +1,11 @@
 package com.foxminded.university.config;
 
-import org.hibernate.SessionFactory;
-import org.springframework.boot.SpringBootConfiguration;
 import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.orm.hibernate5.LocalSessionFactoryBean;
 import org.thymeleaf.spring5.templateresolver.SpringResourceTemplateResolver;
 import org.thymeleaf.templatemode.TemplateMode;
-
-import javax.sql.DataSource;
-import java.io.IOException;
 
 @Configuration
 @EnableConfigurationProperties
--- src/main/java/com/foxminded/university/dao/HolidayRepository.java
+++ src/main/java/com/foxminded/university/dao/HolidayRepository.java
@@ -4,7 +4,6 @@ import com.foxminded.university.model.Holiday;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
 import java.time.LocalDate;
