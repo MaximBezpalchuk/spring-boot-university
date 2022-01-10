93e6a6a15172597ca8d1dc0296f7bab4f2a76a79
Maksimgrom <hjfhjfhjf12>
2022-01-11 01:39:33 +0300

    change error message for each entity

--- src/main/java/com/foxminded/university/validation/AgeValidator.java
+++ src/main/java/com/foxminded/university/validation/AgeValidator.java
@@ -12,10 +12,15 @@ import java.time.Period;
 public class AgeValidator implements ConstraintValidator<Age, Person> {
 
     @Override
-    public boolean isValid(Person person, ConstraintValidatorContext constraintValidatorContext) {
+    public boolean isValid(Person person, ConstraintValidatorContext context) {
+        context.disableDefaultConstraintViolation();
         if (person instanceof Student) {
+            context.buildConstraintViolationWithTemplate("Student`s age should be more than 14!").addConstraintViolation();
+
             return Period.between(person.getBirthDate(), LocalDate.now()).getYears() > 14;
-        } else if (person instanceof Teacher){
+        } else if (person instanceof Teacher) {
+            context.buildConstraintViolationWithTemplate("Teacher`s age should be more than 20!").addConstraintViolation();
+
             return Period.between(person.getBirthDate(), LocalDate.now()).getYears() > 20;
         }
 
