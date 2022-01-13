b513dc2d4cd2d7551516c8cc0fd75a69119cd7fa
Maksimgrom94 <maksim_grom@mail.ru>
2022-01-13 10:18:42 +0300

    resolving Task20#2 issues

--- src/main/java/com/foxminded/university/validation/PhoneNumber.java
+++ src/main/java/com/foxminded/university/validation/PhoneNumber.java
@@ -2,10 +2,12 @@ package com.foxminded.university.validation;
 
 import javax.validation.Constraint;
 import javax.validation.Payload;
+import javax.validation.constraints.Pattern;
 import java.lang.annotation.*;
 
+@Pattern(regexp = "^[0-9]{1,11}$")
 @Documented
-@Constraint(validatedBy = {PhoneNumberValidator.class})
+@Constraint(validatedBy = {})
 @Target({ElementType.FIELD})
 @Retention(RetentionPolicy.RUNTIME)
 public @interface PhoneNumber {
